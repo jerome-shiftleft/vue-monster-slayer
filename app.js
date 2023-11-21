47d9f8fef813af11adc52f8dbdea3d3239df427e
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-21 20:57:06 +0800

    progress

--- app.js
+++ app.js
@@ -4,19 +4,35 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       playerMinDmg: 5,
-      playerMaxDmg: 12
+      playerMaxDmg: 12,
+      monsterMinDmg: 8,
+      monsterMaxDmg: 15
     }
-  },  
+  },
+  watch: {
+
+  },
   methods: {
     attackMonster() {
       console.log('attacking monster!');
       let randomDecimal = Math.random();
       let playerDmg = randomDecimal * (this.playerMaxDmg - this.playerMinDmg + 1) + this.playerMinDmg;
       playerDmg = Math.round(playerDmg);
-      console.log('player damage: ', playerDmg);      
+      console.log(`player's damage: ${playerDmg}`);
       this.monsterHealth -= playerDmg;
       this.monsterHealth = (this.monsterHealth < 0) ? 0 : this.monsterHealth;
       console.log(`monster's health: ${this.monsterHealth}`);
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      console.log('attacking player!');
+      let randomDecimal = Math.random();
+      let monsterDmg = randomDecimal * (this.monsterMaxDmg - this.monsterMinDmg + 1) + this.monsterMinDmg;
+      monsterDmg = Math.round(monsterDmg);
+      console.log(`monster's damage: ${monsterDmg}`);
+      this.playerHealth -= monsterDmg;
+      this.playerHealth = (this.playerHealth < 0) ? 0 : this.playerHealth;
+      console.log(`player's health: ${this.playerHealth}`);
     }
   }
 });
