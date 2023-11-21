32ca9cb495209f6e8a251a454a7ab9e85352f714
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-22 00:15:32 +0800

    balancing win/lose rate

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-function randomAtkDmg(min, max) {
+function getRandomValue(min, max) {
   let randomDecimal = Math.random();
-  let damage = randomDecimal * (max - min) + min;
-  damage = Math.round(damage);
-  return damage;
+  let value = randomDecimal * (max - min) + min;
+  value = Math.round(value);
+  return value;
 }
 
 const app = Vue.createApp({
@@ -14,11 +14,12 @@ const app = Vue.createApp({
       playerMaxDmg: 12,
       monsterMinDmg: 8,
       monsterMaxDmg: 15,
-      specialAtkMin: 10,
+      specialAtkMin: 20,
       specialAtkMax: 25,
       specialAtkExecuted: false,
-      healExecuted: 0,
-      healRestore: 25,
+      healExecuted: false,
+      minHeal: 20,
+      maxHeal: 25,
       winner: null,
       currentRound: 0
     }
@@ -34,8 +35,12 @@ const app = Vue.createApp({
         width: `${this.monsterHealth}%`
       }
     },
-    mayUseSpecialAttack() {
-      return this.currentRound % 3 !== 0;
+    mayNotUseSpecialAttack() {
+      //return this.currentRound % 3 !== 0;
+      return this.specialAtkExecuted;
+    },
+    mayNotHeal() {
+      return (this.currentRound === 0 || this.healExecuted) ? true : false;
     }
   },
   watch: {
@@ -57,7 +62,7 @@ const app = Vue.createApp({
     attackMonster() {
       console.log('attacking monster!');
       this.currentRound++;
-      let playerDmg = randomAtkDmg(this.playerMinDmg, this.playerMaxDmg)
+      let playerDmg = getRandomValue(this.playerMinDmg, this.playerMaxDmg)
       console.log(`player's damage: ${playerDmg}`);
       this.monsterHealth -= playerDmg;
       this.monsterHealth = (this.monsterHealth < 0) ? 0 : this.monsterHealth;
@@ -69,7 +74,7 @@ const app = Vue.createApp({
     specialAtk() {
       console.log('special attack!');
       this.currentRound++;
-      let playerDmg = randomAtkDmg(this.specialAtkMin, this.specialAtkMax)
+      let playerDmg = getRandomValue(this.specialAtkMin, this.specialAtkMax)
       console.log(`player's damage: ${playerDmg}`);
       this.monsterHealth -= playerDmg;
       this.monsterHealth = (this.monsterHealth < 0) ? 0 : this.monsterHealth;
@@ -81,16 +86,17 @@ const app = Vue.createApp({
     },
     attackPlayer() {
       console.log('attacking player!');
-      let monsterDmg = randomAtkDmg(this.monsterMinDmg, this.monsterMaxDmg)
+      let monsterDmg = getRandomValue(this.monsterMinDmg, this.monsterMaxDmg)
       console.log(`monster's damage: ${monsterDmg}`);
       this.playerHealth -= monsterDmg;
       this.playerHealth = (this.playerHealth < 0) ? 0 : this.playerHealth;
       console.log(`player's health: ${this.playerHealth}`);
     },
     heal() {
-      this.playerHealth += this.healRestore;
+      this.currentRound++;
+      this.playerHealth += getRandomValue(this.minHeal, this.maxHeal);
       this.playerHealth = this.playerHealth > 100 ? 100 : this.playerHealth;
-      this.healExecuted += 1;
+      this.healExecuted = true;
       this.attackPlayer();
     },
     battleResult(winner) {
@@ -107,7 +113,7 @@ const app = Vue.createApp({
       this.monsterHealth = 100;
       this.currentRound = 0;
       this.specialAtkExecuted = false;
-      this.healExecuted = 0;
+      this.healExecuted = false;
     }
   } // end of methods
 });
